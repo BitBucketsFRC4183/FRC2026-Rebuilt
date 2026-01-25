6fc840077d30118099727fbb69f0fe9aeba0fae2
Navya <navyazhni@gmail.com>
2026-01-24 20:34:40 -0700

    shooter hood code, still need to fix motor stuff

--- src/main/java/frc/robot/subsystems/auto/AutoSubsystem.java
+++ src/main/java/frc/robot/subsystems/auto/AutoSubsystem.java
@@ -1,28 +1,33 @@
 package frc.robot.subsystems.auto;
 
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.config.RobotConfig;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.*;
+
 
 import frc.robot.subsystems.drive.DriveSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class AutoSubsystem extends SubsystemBase {
 
   private final DriveSubsystem drive;
+  private final ShooterSubsystem shooter;
 
-  public AutoSubsystem(DriveSubsystem drive) {
+  public AutoSubsystem(DriveSubsystem drive, ShooterSubsystem shooter) {
     this.drive = drive;
-
+    this.shooter = shooter;
     configureAutoBuilder();
     registerNamedCommands();
   }
 
+  PathPlannerPath MidStarttoMid = PathPlannerPath.fromChoreoTrajectory("MidStarttoMid");
   //Setup PathPlanner
 
   private void configureAutoBuilder() {
@@ -68,12 +73,89 @@ public class AutoSubsystem extends SubsystemBase {
   }
 
   public Command shoot() {
-    return Commands.print("Shooting!");
-    // replace with real shooter command
+    return Commands.print("shooting!")
   }
 
   public Command climb() {
     return Commands.print("Climbing!");
     // replace with real climb command
+  }
+
+  //autoroutines
+  public Command goBottomToMid(){
+    PathPlannerPath path1 = PathPlannerPath.fromChoreoTrajectory("BottomStartToMid");
+
+    return AutoBuilder.followPath(path1);
+  }
+  public Command goMidtoTower(){
+    PathPlannerPath path2 = PathPlannerPath.fromChoreoTrajectory("MidtoTower");
+
+    return AutoBuilder.followPath(path2);
+  }
+
+  public Command goToptoMid(){
+    PathPlannerPath path3 = PathPlannerPath.fromChoreoTrajectory("TopStarttoMid");
+  }
+
+  public Command goMidtoDepot(){
+    PathPlannerPath path4 = PathPlannerPath.fromChoreoTrajectory("MidtoDepot");
+  }
+
+  public Command goDepotToMid(){
+    PathPlannerPath path5 = PathPlannerPath.fromChoreoTrajectory("DepotToMid");
+  }
+
+  public Command goMidToTower(){
+    PathPlannerPath path6  = PathPlannerPath.fromChoreoTrajectory("MidStarttoTower");
+  }
+
+  public Command goMidToMid(){
+    PathPlannerPath path7 = PathPlannerPath.fromChoreoTrajectory("MidStarttoMid");
+  }
+  public Command bottomToMidAuto(){
+    return Commands.sequence(
+            new InstantCommand(() -> System.out.println("Bottom to Mid starting")),
+            goBottomToMid(),
+            new InstantCommand(()-> System.out.println("Reached mid position")),
+
+    );
+
+  }
+
+  public Command StartBottomShootEndTower(){
+    return Commands.sequence(
+            new InstantCommand(()-> System.out.println("Routine 1 starting")),
+            goBottomToMid(),
+            shoot(),
+            goMidtoDepot(),
+            new WaitCommand(3),
+            goDepotToMid(),
+            goMidToTower(),
+            climb(),
+            stop()
+            new InstantCommand(()-> System.out.println("routine 1 complete"));
+
+    )
+  }
+
+  public Command StartMidShootIntakeTower(){
+    return Commands.sequence(
+            new InstantCommand(()->System.out.println("Moving to Shooter position"));
+            goMidToMid(),
+            shoot(),
+            goMidtoDepot(),
+            new WaitCommand(3),
+            goDepotToMid(),
+            goMidToTower(),
+            climb(),
+            stop()
+            new InstantCommand(()->System.out.println("complete routine"));
+            )
+  }
+
+  public Command StartTopShootIntakeTower(){
+    return Commands.sequence(
+            new InstantCommand(()-> System.out.print)
+    )
   }
 }
