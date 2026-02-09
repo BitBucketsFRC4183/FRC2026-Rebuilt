9acc74488d395135a571d976e364bc179cc9be38
hongyu-long <ms.long.life@gmail.com>
2026-02-08 19:59:29 -0700

    getTheDistance dada

--- src/main/java/frc/robot/constants/AprilTagLabel.java
+++ src/main/java/frc/robot/constants/AprilTagLabel.java
@@ -11,12 +11,14 @@ public class AprilTagLabel {
     public static final int BLUE_RIGHT_BELOW_HUB = 26;
     public static final int RED_LITTLE_SIDE_BELOW_HUB =9;
     public static final int BLUE_LITTLE_SIDE_BELOW_HUB =25;
+
     //offset, it is the center of hopper
+    //estimation is estimation, actual needs to tuned on field
+    //They should be very accurate
     public static final Transform3d tagToHub3d = new Transform3d(
             new Translation3d(0.2,0, 0.6096),
             new Rotation3d()
     );
-    public static final Pose3d RedHubPose3d = aprilTagFieldLayout.getTagPose(RED_RIGHT_BELOW_HUB).plus(tagToHub3d);
-    public static final Pose3d BlueHubPose3d = aprilTagFieldLayout.getTagPose(BLUE_RIGHT_BELOW_HUB).plus(tagToHub3d) ;
-    Pose3d targetHubPose = (DriverStation.Alliance.Red) ? RedHubPose3d : BlueHubPose3d;
+    public static final Pose3d RedHubPose3d = aprilTagFieldLayout.getTagPose(RED_RIGHT_BELOW_HUB).get().plus(tagToHub3d);
+    public static final Pose3d BlueHubPose3d = aprilTagFieldLayout.getTagPose(BLUE_RIGHT_BELOW_HUB).get().plus(tagToHub3d);
 }
--- src/main/java/frc/robot/subsystems/vision/HopperTracker.java
+++ src/main/java/frc/robot/subsystems/vision/HopperTracker.java
@@ -1,10 +1,22 @@
 package frc.robot.subsystems.vision;
 
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.constants.AprilTagLabel;
 
 public class HopperTracker {
+    /// fancy notation
+    /// function that give you hub pose based on alliance color
+    public static Pose3d getTargetHubPose3d() {
+        var alliance = DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue);
+        return (alliance == DriverStation.Alliance.Red)
+                ? AprilTagLabel.RedHubPose3d
+                : AprilTagLabel.BlueHubPose3d;
+    }
 
-    //hopper top to center of the apriltag = (estimate) 2 foot
-//estimation is estimation, actual needs to tuned on field
-
+    public static double getDistanceFromRobotToHub(Pose2d robotPose) {
+        Pose2d hubPose2d = getTargetHubPose3d().toPose2d();
+return robotPose.getTranslation()
+        .getDistance(hubPose2d.getTranslation());
+    }
 }
