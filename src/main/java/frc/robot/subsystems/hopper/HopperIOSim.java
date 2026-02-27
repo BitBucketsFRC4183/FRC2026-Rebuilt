a0d3e75eac4496bec85b27e1535a649155d948c5
Tristan Rubio <triskit68@gmail.com>
2026-02-27 12:56:40 -0700

    Hopper Volt

--- src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIOSim.java
@@ -1,39 +1,24 @@
 package frc.robot.subsystems.hopper;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.Timer;
 
 public class HopperIOSim implements HopperIO {
 
-  private double motorVelocityRPS = 0.0;
-  private double motorCurrent = 0.0;
-  private double targetVelocity = 0.0;
-
-  private static final double MAX_RPS = 100.0;
-  private static final double MAX_CURRENT = 40.0;
-  private static final double RESPONSE = 10.0;
-
-  private double lastTime = Timer.getFPGATimestamp();
+  private double conveyorPercent = 0.0;
 
   @Override
   public void updateInputs(HopperIOInputs inputs) {
-    double now = Timer.getFPGATimestamp();
-    double dt = now - lastTime;
-    lastTime = now;
-
-    double target = targetVelocity;
-
-    motorVelocityRPS += (target - motorVelocityRPS) * MathUtil.clamp(dt * RESPONSE, 0.0, 1.0);
-
-    motorCurrent = Math.abs(motorVelocityRPS / MAX_RPS) * MAX_CURRENT;
-
-    inputs.motorVelocityRPS = motorVelocityRPS;
-    inputs.motorCurrentAmps = motorCurrent;
-    inputs.motorTargetVelocityRPS = targetVelocity;
+    // In sim, applied output is just the commanded percent
+    inputs.conveyorAppliedOutput = conveyorPercent;
   }
 
   @Override
-  public void setVelocity(double rps) {
-    targetVelocity = MathUtil.clamp(rps, -MAX_RPS, MAX_RPS);
+  public void setConveyorPercent(double percent) {
+    conveyorPercent = MathUtil.clamp(percent, -1.0, 1.0);
+  }
+
+  @Override
+  public void stopConveyor() {
+    conveyorPercent = 0.0;
   }
 }
