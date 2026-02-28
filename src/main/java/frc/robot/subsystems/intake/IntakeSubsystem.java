554577b8a49534d25f7ed17429ed95f2046fb5c6
Tristan Rubio <triskit68@gmail.com>
2026-02-28 14:59:18 -0700

    Sendable Chooser for IntakeSubsystem

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.IntakeConstants;
+import frc.robot.subsystems.vision.VisionMode;
 import org.littletonrobotics.junction.Logger;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -11,9 +14,18 @@ public class IntakeSubsystem extends SubsystemBase {
 
   private IntakeState currentState = IntakeState.DEPLOYED;
 
+  private final SendableChooser<IntakeState> intakeStateChooser = new SendableChooser<>();
+
   public IntakeSubsystem(IntakeIO io) {
     this.io = io;
+    intakeStateChooser.addOption("STOWED", IntakeState.STOWED);
+    intakeStateChooser.addOption("DEPLOYED", IntakeState.DEPLOYED);
+    intakeStateChooser.addOption("INTAKING", IntakeState.INTAKING);
+    intakeStateChooser.addOption("OUTTAKING", IntakeState.OUTTAKING);
+
+    SmartDashboard.putData("IntakeStateChooser", intakeStateChooser);
   }
+
 
   @Override
   public void periodic() {
@@ -47,6 +59,10 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     Logger.processInputs("Intake", inputs);
+
+    IntakeState manualSelectMode = intakeStateChooser.getSelected();
+    currentState = (manualSelectMode);
+
   }
 
   public boolean isRunning() {
