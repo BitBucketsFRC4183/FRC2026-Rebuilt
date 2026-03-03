2f3835876d4e5e6a4ea374445b906cfceda74dd6
JustinTheDuck <justin.xuan.hsu@gmail.com>
2026-03-02 19:27:15 -0700

    Combining StoredDistance with VisionShoot

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -6,27 +6,19 @@ import frc.robot.subsystems.hopper.HopperSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class ShooterCommands {
-  public static Command storeDistance(ShooterSubsystem shooterSubsystem, double distance) {
-    return Commands.runOnce(
-        () -> {
-          shooterSubsystem.setStoredDistance(distance);
-          shooterSubsystem.calculateVelocity();
-        });
-  }
-
   public static Command shootAtRPS(double targetVelocity, ShooterSubsystem shooterSubsystem, HopperSubsystem hopperSubsystem) {
     return Commands.sequence( Commands.runOnce(
                     () -> shooterSubsystem.setTargetVelocity(targetVelocity)),
             Commands.waitUntil(shooterSubsystem::targetReached).andThen(feed(shooterSubsystem, hopperSubsystem)));
   }
 
-  public static Command visionShoot(
+  public static Command visionShoot(double distance,
       ShooterSubsystem shooterSubsystem, HopperSubsystem hopperSubsystem) {
-    return Commands.sequence(
-        // Waits for the distance from vision
-        Commands.waitUntil(shooterSubsystem::distanceStored)
-        // Runs the flywheel until the controller is released
-            .until(shooterSubsystem::targetReached)
+    return Commands.sequence(Commands.runOnce(
+            () -> {
+              shooterSubsystem.setStoredDistance(distance);
+              shooterSubsystem.calculateVelocity();
+            }).until(shooterSubsystem::targetReached)
             .andThen(feed(shooterSubsystem, hopperSubsystem)));
   }
 
