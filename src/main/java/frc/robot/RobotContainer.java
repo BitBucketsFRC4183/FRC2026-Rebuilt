67a65bcf713572953fa87eef79ea6181cdcac4b9
hongyu-long <hongyulong32@gmail.com>
2026-03-05 12:26:04 -0700

    naming change

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -290,7 +290,7 @@ public class RobotContainer {
         driveSubsystem,
         () -> -driverController.getLeftY(),
         () -> -driverController.getLeftX(),
-        () -> visionSubsystem.getAimAngle());
+        () -> visionSubsystem.getAimTargetAngle());
   }
 
   public Command getAutonomousCommand() {
