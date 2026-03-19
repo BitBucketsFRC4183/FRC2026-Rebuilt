688676189333a8a879c803d9fa48ca0918a54261
Titus Nguyen <titus.tnguyen17@gmail.com>
2026-03-19 08:48:45 -0700

    change max speed

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -97,7 +97,7 @@ public class TunerConstants {
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
   // 19.2 m/s to 5.33ft/s
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.33);
+  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.12);
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
