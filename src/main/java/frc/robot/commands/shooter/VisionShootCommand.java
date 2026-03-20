23dc60048e11e6921516c003754f4fab10f4d4c1
JustinTheDuck <justin.xuan.hsu@gmail.com>
2026-03-19 19:20:15 -0700

    Clean up

--- src/main/java/frc/robot/commands/shooter/VisionShootCommand.java
+++ src/main/java/frc/robot/commands/shooter/VisionShootCommand.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.hopper.HopperSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
@@ -13,7 +12,6 @@ public class VisionShootCommand extends Command {
   private final Drive drive;
   private final VisionSubsystem vision;
   private boolean charged = false;
-  private boolean stop = false;
 
   public VisionShootCommand(
       ShooterSubsystem shooter, HopperSubsystem hopper, Drive drive, VisionSubsystem vision) {
@@ -29,37 +27,21 @@ public class VisionShootCommand extends Command {
 
   @Override
   public void initialize() {
-    System.out.println("Started");
     charged = true;
-    stop = false;
     shooter.setStoredDistance(vision.getHubDistanceMeter(drive::getPose));
   }
 
   @Override
   public void execute() {
     if (charged) {
-      Commands.runOnce(shooter::charge);
+      shooter.charge();
       charged = false;
     } else {
-      Commands.runOnce(shooter::calculateVelocity);
+      shooter.calculateVelocity();
     }
     if (shooter.targetReached()) {
       ShooterCommands.startFeeding(shooter, hopper);
-      stop = true;
+      end(true);
     }
-  }
-
-  @Override
-  public boolean isFinished() {
-    return stop;
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    System.out.println("Command Finished");
-    shooter.resetStoredDistance();
-    shooter.stopFlywheel();
-    shooter.stopIntermediateMotor();
-    hopper.stopConveyor();
   }
 }
